62eaa6b6f36148fb5ecbe7aa1cddbf34a24f412c
H. Kamran <hkamran@unisontech.org>
2019-11-05 12:33:26 -0800

    Release V2
    Removed ALL of the calculation code, ported it to JS

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 Schedules
 Contributors:
 	:: H. Kamran [@hkamran80] (author)
-Version: 1.1.5
-Last Updated: 2019-11-04, @hkamran80
+Version: 2.0.0
+Last Updated: 2019-11-05, @hkamran80
 """
 
 from flask import Flask, render_template, request, redirect, url_for
 import period_calculator
-import pt_extensions
 import sys
 import os
 
@@ -27,68 +26,18 @@ def school():
 @app.route("/schedule/<schedule_id>", methods=["GET"])
 def schedule(schedule_id):
 	if request.method == "GET":
-		#schoolid = request.args.get("schoolid") or None
-		schoolname = schedules[schedule_id]
+		schedule_name = schedules[schedule_id]
 
 		if not schedule_id:
 			return redirect("/")
 
-		days = {"MON": "Monday", "TUE": "Tuesday", "WED": "Wednesday", "THU": "Thursday", "FRI": "Friday", "SAT": "Saturday", "SUN": "Sunday"}
+		if request.args.get("mode") == "dev":
+            mode = "dev"
+        else:
+            mode = None
 
-		if request.args.get("time") and request.args.get("day"):
-			if request.args.get("mode") == "dev":
-				mode = "dev"
-			else:
-				mode = None
-
-			day = request.args.get("day")
-			full_day = days[request.args.get("day")]
-
-			string_time = str(request.args.get("time"))
-
-			hours, minutes = [int(t) for t in string_time.split("-")]
-			time = int(pt_extensions.zero_time(hours) + pt_extensions.zero_time(minutes))
-
-			period, time_data = period_calculator.get_current_period(schedule_id, day, time)
-			print(period, "//", time_data)
-
-			if time_data == "No Time":
-				time_difference = "NT"
-				compiled_time_diff = ""
-			else:
-				print(time_data[1], "//", int(str(time_data[1])[:2]) in range(1, 24), "//", str(time_data[1])[:2])
-				if int(str(time_data[1])[:2]) not in range(1, 24):
-					print(int(str(time_data[1])[0]), int(str(time_data[1])[1:]))
-					scheduled_end = f"{int(str(time_data[1])[0])}-{int(str(time_data[1])[1:])}"
-				else:
-					print(int(str(time_data[1])[:2]), int(str(time_data[1])[2:]))
-					scheduled_end = f"{int(str(time_data[1])[:2])}-{int(str(time_data[1])[2:])}"
-
-				time_difference = pt_extensions.calculate_time(str(string_time), str(scheduled_end))
-				# (time_data[1] = 920) =(ct)=> 92:0 ≠ 9:20
-				# time_data[1] (920) ==> (9-20)
-				print(period, "//", time_data, "//", time_difference)
-
-				if time_difference[0] != 0:
-					compiled_time_diff = f"{time_difference[0]}h{time_difference[1]}m"
-				else:
-					compiled_time_diff = f"{time_difference[1]}m"
-
-			if len(string_time.split("-")[0]) == 1:
-				hours = "0" + str(string_time.split("-")[0])
-			else:
-				hours = str(string_time.split("-")[0])
-
-			if len(string_time.split("-")[1]) == 1:
-				minutes = "0" + str(string_time.split("-")[1])
-			else:
-				minutes = str(string_time.split("-")[1])
-
-			print(hours, "//", minutes)
-
-			return render_template("index.html", schedule_valid=True, schedules=schedules, schedule_id=schedule_id, schedule_name=schoolname, schedule_color=schedule_id, period=period, day=full_day, time=f"{hours}:{minutes}", get_time_date=False, time_data=time_data, time_diff_raw=time_difference, time_diff=compiled_time_diff, raw=mode)
-		else:
-			return render_template("index.html", schedule_valid=True, schedule_id=schedule_id, schoolname=schoolname, get_time_date=True)
+        #return render_template("index.html", schedule_valid=True, schedules=schedules, schedule_id=schedule_id, schedule_name=schedule_name, schedule_color=schedule_id, period=period, day=full_day, time=f"{hours}:{minutes}", get_time_date=False, time_data=time_data, time_diff_raw=time_difference, time_diff=compiled_time_diff, raw=mode)
+        return render_template("index.html", schedule_valid=True, schedule_id=schedule_id, schedule_name=schedule_name, schedule_color=schedule_id, raw_schedule_json=json.dumps(period_calculator.times), schedule_id=schedule_id)
 
 @app.route("/timecalc", methods=["GET"])
 def timecalc():
