68723be0ae6eef2d0a9fa98def1d3b557807f07f
H. Kamran <hkamran@unisontech.org>
2019-11-07 10:12:01 -0800

    Update times

--- period_calculator.py
+++ period_calculator.py
@@ -12,68 +12,84 @@ times = {
 	"auhsd-ahs": {
 		"MON": {
 			"1st Period": (835, 920),
-			"Passing: 1/2": (921, 924),
+			"Passing: 1/2": (920, 925),
 			"2nd Period": (925, 1010),
-			"Passing: 2/3": (1011, 1014),
+			"Passing: 2/3": (1010, 1015),
 			"3rd Period": (1015,1100),
 			"Brunch": (1100, 1110),
-			"Passing: Brunch/4": (1101, 1114),
+			"Passing: Brunch/4": (1110, 1115),
 			"4th Period": (1115, 1200),
-			"Passing: 4/5": (1201, 1204),
+			"Passing: 4/5": (1200, 1205),
 			"5th Period": (1205, 1250),
 			"Lunch": (1250, 1325),
-			"Passing: Lunch/6": (1326, 1329),
+			"Passing: Lunch/6": (1325, 1330),
 			"6th Period": (1330, 1415),
-			"Passing: 6/7": (1416, 1419),
+			"Passing: 6/7": (1415, 1420),
 			"7th Period": (1420, 1505)
 		},
 		"TUE": {
 			"1st Period": (800, 930),
-			"Passing: 1/2": (930, 939),
+			"Passing: 1/2": (930, 940),
 			"2nd Period": (940, 1110),
-			"Brunch": (1100, 1116),
-			"Passing: Brunch/3": (1116, 1124),
-			"3rd Period": (1124, 1255),
-			"Lunch": (1255, 1326),
-			"Passing: Lunch/7": (1326, 1334),
-			"7th Period": (1334, 1505)
+			"Brunch": (1100, 1115),
+			"Passing: Brunch/3": (1115, 1125),
+			"3rd Period": (1125, 1255),
+			"Lunch": (1255, 1325),
+			"Passing: Lunch/7": (1325, 1335),
+			"7th Period": (1335, 1505)
 		},
 		"WED": {
-			"4th Period": (835, 1005),
-			"Passing: 4/Academy": (1006, 1014),
+			"4th Period": (800, 1005),
+			"Passing: 4/Academy": (1005, 1015),
 			"Academy": (1015, 1110),
 			"Brunch": (1110, 1115),
-			"Passing: Brunch/5": (1116, 1124),
+			"Passing: Brunch/5": (1115, 1125),
 			"5th Period": (1125, 1255),
 			"Lunch": (1255, 1325),
-			"Passing: Lunch/6": (1326, 1334),
+			"Passing: Lunch/6": (1325, 1335),
 			"6th Period": (1335, 1505)
 		},
 		"THU": {
 			"1st Period": (800, 930),
-			"Passing: 1/2": (930, 939),
+			"Passing: 1/2": (930, 940),
 			"2nd Period": (940, 1110),
-			"Brunch": (1100, 1116),
-			"Passing: Brunch/3": (1116, 1124),
-			"3rd Period": (1124, 1255),
-			"Lunch": (1255, 1326),
-			"Passing: Lunch/7": (1326, 1334),
-			"7th Period": (1334, 1505)
+			"Brunch": (1100, 1115),
+			"Passing: Brunch/3": (1115, 1125),
+			"3rd Period": (1125, 1255),
+			"Lunch": (1255, 1325),
+			"Passing: Lunch/7": (1325, 1335),
+			"7th Period": (1335, 1505)
 		},
 		"FRI": {
-			"4th Period": (835, 1005),
-			"Passing: 4/Academy": (1006, 1014),
+			"4th Period": (800, 1005),
+			"Passing: 4/Academy": (1005, 1015),
 			"Academy": (1015, 1110),
 			"Brunch": (1110, 1115),
-			"Passing: Brunch/5": (1116, 1124),
+			"Passing: Brunch/5": (1115, 1125),
 			"5th Period": (1125, 1255),
 			"Lunch": (1255, 1325),
-			"Passing: Lunch/6": (1326, 1334),
+			"Passing: Lunch/6": (1325, 1335),
 			"6th Period": (1335, 1505)
 		}
-	}
+	},
+    "average-work-day": {
+        "MON": {
+            "Work": (900, 1700)
+        },
+        "TUE": {
+            "Work": (900, 1700)
+        },
+        "WED": {
+            "Work": (900, 1700)
+        },
+        "THU": {
+            "Work": (900, 1700)
+        },
+        "FRI": {
+            "Work": (900, 1700)
+        },
+    }
 }
-
 def pt_check(expected_value, variable_to_check):
 	if variable_to_check[1] > current_time < variable_to_check[0]:
 		return True
