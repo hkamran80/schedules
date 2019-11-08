cfe3c85fb2767f0ad2ccc02a023934bb0c8cb01c
H. Kamran <hkamran@unisontech.org>
2019-11-08 10:41:18 -0800

    Create times.py, add "auhsd-chs"
    [skip travis]

---
+++ times.py
@@ -0,0 +1,145 @@
+times = {
+	"auhsd-ahs": {
+		"MON": {
+			"1st Period": (835, 920),
+			"Passing: 1/2": (920, 925),
+			"2nd Period": (925, 1010),
+			"Passing: 2/3": (1010, 1015),
+			"3rd Period": (1015,1100),
+			"Brunch": (1100, 1110),
+			"Passing: Brunch/4": (1110, 1115),
+			"4th Period": (1115, 1200),
+			"Passing: 4/5": (1200, 1205),
+			"5th Period": (1205, 1250),
+			"Lunch": (1250, 1325),
+			"Passing: Lunch/6": (1325, 1330),
+			"6th Period": (1330, 1415),
+			"Passing: 6/7": (1415, 1420),
+			"7th Period": (1420, 1505)
+		},
+		"TUE": {
+			"1st Period": (800, 930),
+			"Passing: 1/2": (930, 940),
+			"2nd Period": (940, 1110),
+			"Brunch": (1100, 1115),
+			"Passing: Brunch/3": (1115, 1125),
+			"3rd Period": (1125, 1255),
+			"Lunch": (1255, 1325),
+			"Passing: Lunch/7": (1325, 1335),
+			"7th Period": (1335, 1505)
+		},
+		"WED": {
+			"4th Period": (800, 1005),
+			"Passing: 4/Academy": (1005, 1015),
+			"Academy": (1015, 1110),
+			"Brunch": (1110, 1115),
+			"Passing: Brunch/5": (1115, 1125),
+			"5th Period": (1125, 1255),
+			"Lunch": (1255, 1325),
+			"Passing: Lunch/6": (1325, 1335),
+			"6th Period": (1335, 1505)
+		},
+		"THU": {
+			"1st Period": (800, 930),
+			"Passing: 1/2": (930, 940),
+			"2nd Period": (940, 1110),
+			"Brunch": (1100, 1115),
+			"Passing: Brunch/3": (1115, 1125),
+			"3rd Period": (1125, 1255),
+			"Lunch": (1255, 1325),
+			"Passing: Lunch/7": (1325, 1335),
+			"7th Period": (1335, 1505)
+		},
+		"FRI": {
+			"4th Period": (800, 1005),
+			"Passing: 4/Academy": (1005, 1015),
+			"Academy": (1015, 1110),
+			"Brunch": (1110, 1115),
+			"Passing: Brunch/5": (1115, 1125),
+			"5th Period": (1125, 1255),
+			"Lunch": (1255, 1325),
+			"Passing: Lunch/6": (1325, 1335),
+			"6th Period": (1335, 1505)
+		}
+	},
+    "auhsd-chs": {
+      "MON": {
+        "1st Period": (835, 920),
+        "Passing: 1/2": (920, 925),
+        "2nd Period": (925, 1010),
+        "Passing: 2/3": (1010, 1015),
+        "3rd Period": (1015,1100),
+        "Brunch": (1100, 1110),
+        "Passing: Brunch/4": (1110, 1115),
+        "4th Period": (1115, 1200),
+        "Passing: 4/5": (1200, 1205),
+        "5th Period": (1205, 1250),
+        "Lunch": (1250, 1325),
+        "Passing: Lunch/6": (1325, 1330),
+        "6th Period": (1330, 1415),
+        "Passing: 6/7": (1415, 1420),
+        "7th Period": (1420, 1505)
+      },
+      "TUE": {
+        "1st Period": (800, 930),
+        "Passing: 1/2": (930, 940),
+        "2nd Period": (940, 1110),
+        "Brunch": (1100, 1115),
+        "Passing: Brunch/3": (1115, 1125),
+        "3rd Period": (1125, 1255),
+        "Lunch": (1255, 1325),
+        "Passing: Lunch/7": (1325, 1335),
+        "7th Period": (1335, 1505)
+      },
+      "WED": {
+        "4th Period": (800, 1005),
+        "Passing: 4/Academy": (1005, 1015),
+        "Academy": (1015, 1110),
+        "Brunch": (1110, 1115),
+        "Passing: Brunch/5": (1115, 1125),
+        "5th Period": (1125, 1255),
+        "Lunch": (1255, 1325),
+        "Passing: Lunch/6": (1325, 1335),
+        "6th Period": (1335, 1505)
+      },
+      "THU": {
+        "1st Period": (800, 930),
+        "Passing: 1/2": (930, 940),
+        "2nd Period": (940, 1110),
+        "Brunch": (1100, 1115),
+        "Passing: Brunch/3": (1115, 1125),
+        "3rd Period": (1125, 1255),
+        "Lunch": (1255, 1325),
+        "Passing: Lunch/7": (1325, 1335),
+        "7th Period": (1335, 1505)
+      },
+      "FRI": {
+        "4th Period": (800, 1005),
+        "Passing: 4/Academy": (1005, 1015),
+        "Academy": (1015, 1110),
+        "Brunch": (1110, 1115),
+        "Passing: Brunch/5": (1115, 1125),
+        "5th Period": (1125, 1255),
+        "Lunch": (1255, 1325),
+        "Passing: Lunch/6": (1325, 1335),
+        "6th Period": (1335, 1505)
+      }
+    },
+    "average-work-day": {
+      "MON": {
+          "Work": (900, 1700)
+      },
+      "TUE": {
+          "Work": (900, 1700)
+      },
+      "WED": {
+          "Work": (900, 1700)
+      },
+      "THU": {
+          "Work": (900, 1700)
+      },
+      "FRI": {
+          "Work": (900, 1700)
+      }
+    }
+}
