eb8a455771ae96072d563cdf3fde9c30b8e83ec3
H. Kamran <hkamran@unisontech.org>
2020-09-07 15:06:38 -0700

    Update routing, add _redirects file for Netlify

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Schedule from "../views/Schedule.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: "/",
-        name: "Home",
+        name: "home",
         component: Home
     },
     {
         path: "/schedule/:id",
-        name: "Schedule",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "schedule" */ "../views/Schedule.vue")
+        name: "schedule",
+        component: Schedule
     }
 ];
 
