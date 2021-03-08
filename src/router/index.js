3c82cd6a2686d3f4ed290c77b1eb923eafcf3c50
H. Kamran <hkamran@unisontech.org>
2021-03-08 10:43:20 -0800

    Add Netlify headers, update Vue config to include edge naming, prefetch Umami script, add AUHSD hybrid

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "@/views/Home.vue";
+const Home = () => import("@/views/Home.vue");
+// import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
