184f676ccbe4a718fb28b2381bb851ca32c9e0fb
H. Kamran <hkamran@unisontech.org>
2022-11-02 17:49:22 -0700

    Replace colour picking function with `chooseContrastingColour`

--- src/components/ScheduleCard.vue
+++ src/components/ScheduleCard.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
+import { chooseContrastingColour } from "@hkamran/utility-web";
 import type { ScheduleDetails, ScheduleTypes } from "../types/schedule";
 
 const props = defineProps<{
     scheduleId: string;
     schedule: ScheduleTypes;
 }>();
-
-const pickTextColorBasedOnBgColorAdvanced = (
-    bgColor: string,
-    lightColor: string,
-    darkColor: string,
-): string => {
-    const color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
-    const r = parseInt(color.substring(0, 2), 16);
-    const g = parseInt(color.substring(2, 4), 16);
-    const b = parseInt(color.substring(4, 6), 16);
-    const uicolors = [r / 255, g / 255, b / 255];
-    const c = uicolors.map((col) => {
-        if (col <= 0.03928) {
-            return col / 12.92;
-        }
-
-        return Math.pow((col + 0.055) / 1.055, 2.4);
-    });
-    const L = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
-
-    return L > 0.179 ? darkColor : lightColor;
-};
 </script>
 
 <template>
@@ -36,7 +15,7 @@ const pickTextColorBasedOnBgColorAdvanced = (
         class="w-full rounded-lg px-6 py-4 text-left"
         :style="[
             `background-color: ${(schedule as ScheduleDetails).color}`,
-            `color: ${pickTextColorBasedOnBgColorAdvanced(
+            `color: ${chooseContrastingColour(
                 (schedule as ScheduleDetails).color,
                 '#FFFFFF',
                 '#000000',
@@ -50,7 +29,7 @@ const pickTextColorBasedOnBgColorAdvanced = (
         class="w-full rounded-lg px-6 py-4 text-left"
         :style="[
             `background-color: ${(schedule as ScheduleDetails).color}`,
-            `color: ${pickTextColorBasedOnBgColorAdvanced(
+            `color: ${chooseContrastingColour(
                 (schedule as ScheduleDetails).color,
                 '#FFFFFF',
                 '#000000',
