a34e20fd51a7ae39efa542fbe35d6856fc4207e9
H. Kamran <hkamran@unisontech.org>
2022-11-02 20:21:50 -0700

    Add the new credit components

---
+++ src/components/Credit.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { SVGProps } from "lucide-vue-next";
+import { FunctionalComponent } from "vue";
+import BaseCredit from "./BaseCredit.vue";
+
+const props = defineProps<{
+    name: string;
+    credit: string;
+    icon: FunctionalComponent<SVGProps>;
+    url?: string;
+}>();
+</script>
+
+<template>
+    <!-- eslint-disable-next-line vuejs-accessibility/anchor-has-content -->
+    <a
+        v-if="props.url && props.url.trim()"
+        :href="props.url"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <BaseCredit
+            :name="props.name"
+            :credit="props.credit"
+            :icon="props.icon"
+        />
+    </a>
+
+    <BaseCredit
+        v-else
+        :name="props.name"
+        :credit="props.credit"
+        :icon="props.icon"
+    />
+</template>
