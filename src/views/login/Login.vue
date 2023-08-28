083afe4cf55af3716c3e9199b8d8ca27eca84995
H. Kamran <hkamran@hkamran.com>
2023-08-27 22:22:34 -0700

    Add views

---
+++ src/views/login/Login.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import InputField from "../../components/InputField.vue";
+import { supabase } from "../../composables/auth";
+
+const emit = defineEmits(["complete"]);
+
+const emailInput = ref("");
+const passwordInput = ref("");
+const error = ref<string | undefined>();
+
+const login = async () => {
+    const response = await supabase.auth.signInWithPassword({
+        email: emailInput.value,
+        password: passwordInput.value,
+    });
+    console.debug(response);
+
+    if (response.error) {
+        if (response.error.message === "Invalid login credentials") {
+            error.value = "Incorrect email/password combination";
+        } else {
+            error.value = response.error.message;
+        }
+    } else if (response.data.user && response.data.session) {
+        emit("complete");
+    }
+};
+</script>
+
+<template>
+    <form
+        class="space-y-6 rounded-lg bg-white px-8 py-8 shadow dark:bg-ut-grey sm:px-10"
+    >
+        <InputField
+            v-model="emailInput"
+            type="email"
+            :input-props="{ autocomplete: 'email' }"
+            placeholder="Email"
+            label="Email"
+            :error="error"
+            required
+        />
+
+        <InputField
+            v-model="passwordInput"
+            type="password"
+            :input-props="{ autocomplete: 'current-password' }"
+            placeholder="Password"
+            label="Password"
+            :error="error"
+            required
+        />
+
+        <div>
+            <button
+                type="button"
+                :class="[
+                    'group relative flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white transition duration-300',
+                    emailInput.trim() === '' || passwordInput.trim() === ''
+                        ? 'bg-gray-500'
+                        : 'bg-pink-700 hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-pink-700 focus:ring-offset-2 dark:bg-pink-400 dark:focus:ring-pink-400',
+                ]"
+                :disabled="
+                    emailInput.trim() === '' || passwordInput.trim() === ''
+                "
+                @click="login"
+            >
+                Sign in
+            </button>
+        </div>
+
+        <p class="text-xs text-gray-700 dark:text-gray-300">
+            To create an account or reset your password, please visit the
+            <a
+                class="underline transition-colors duration-150 ease-in-out hover:text-pink-700 dark:hover:text-pink-500"
+                href="https://beta-account.unisontech.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >UT Account portal</a
+            >.
+        </p>
+    </form>
+</template>
---
+++ src/views/login/MFA.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { Session } from "@supabase/supabase-js";
+import { computed, ref } from "vue";
+import { supabase } from "../../composables/auth";
+import InputField from "../../components/InputField.vue";
+
+const props = defineProps<{ session: Session }>();
+const emit = defineEmits(["recheck"]);
+
+const otp = ref("");
+const otpErrorText = ref();
+const preventProceed = computed(
+    () => otp.value.trim().length !== 6 || isNaN(Number(otp)),
+);
+
+const factor = props.session.user.factors![0];
+
+const verifyMFA = async () => {
+    const { error } = await supabase.auth.mfa.challengeAndVerify({
+        factorId: factor.id,
+        code: otp.value,
+    });
+
+    if (error) {
+        otpErrorText.value = "OTP code is incorrect";
+    } else {
+        emit("recheck");
+    }
+};
+</script>
+
+<template>
+    <div
+        class="space-y-6 rounded-lg bg-white px-8 py-8 shadow dark:bg-ut-grey sm:px-10"
+    >
+        <InputField
+            v-model="otp"
+            type="text"
+            placeholder="OTP Code"
+            label="OTP Code"
+            :input-props="{
+                // inputMode: 'numeric',
+                // autoComplete: 'one-time-code',
+                // maxLength: 6,
+                // onKeyDown: (event: KeyboardEvent) => {
+                //     if (event.key === 'Enter' && !preventProceed) {
+                //         verifyMFA();
+                //     }
+                // },
+            }"
+            required
+            :error="otpErrorText"
+            @keyup.enter="verifyMFA"
+        />
+
+        <div class="space-y-4">
+            <button
+                type="button"
+                class="[
+                'group duration-300',
+                preventProceed ? 'bg-gray-500' : 'bg-pink-700 dark:focus:ring-pink-400', 'relative dark:bg-pink-400 flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white transition hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-pink-700 focus:ring-offset-2']"
+                :disabled="preventProceed"
+                @click="verifyMFA"
+            >
+                Sign in
+            </button>
+
+            <button
+                type="button"
+                class="w-full text-center text-sm text-red-500"
+                @click="supabase.auth.signOut"
+            >
+                Sign out
+            </button>
+        </div>
+    </div>
+</template>
